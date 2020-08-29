1ef27fe7771d109301b4597463b1cbabd10e663c
RK <RK@RkPin2>
2020-08-29 20:13:51 +0530

    First Text

---
+++ src/main/java/com/rk/EmployeManagementRcWithSbApplication.java
@@ -0,0 +1,13 @@
+package com.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeManagementRcWithSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeManagementRcWithSbApplication.class, args);
+	}
+
+}
